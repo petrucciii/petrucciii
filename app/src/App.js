4e4054975a4c2d65c6233abd028e4ad5c1f3093d
petrucciii <riccardo.petrucci99@gmail.com>
2021-04-04 20:03:39 +0200

    only_it

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
 import {Helmet} from "react-helmet";
-import HomeIt from './components/home_it';
-import HomeEn from './components/home_en';
-import ErrorIt from './components/error_it';
-import ErrorEn from './components/error_en';
+import HomeIt from './components/home';
+import ErrorIt from './components/error';
 import Header from './components/header';
 
 function App() {
