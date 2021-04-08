a77b837f01923473954299ba13bc5a38a3da6a23
petrucciii <riccardo.petrucci99@gmail.com>
2021-04-08 18:29:10 +0200

    form-init

--- app/src/components/contact.js
+++ app/src/components/contact.js
@@ -7,10 +7,25 @@ class Contact extends Component{
     return (
       <div>
         <div className="contact">
-            <div className="card">
+            <div className="card-contact">
                 <div className="card_content">
                     <img src={contact} alt="CONTATTI" style={{width: "100px", height: "100px"}}/>
                     <h4 className="card_title" align="center">CONTATTI</h4>
+                    <div className="form">
+                      <form className="form-contact" action="">
+                        <label className="text">Nome</label>
+                        <br/>
+                        <input type="text" className="name" Name="name"></input>
+                        <br/>
+                        <label className="text">E-Mail</label>
+                        <br/>
+                        <input type="e-mail" className="e-mail" Name="e-mail"></input>
+                        <br/>
+                        <label className="text">Oggetto</label>
+                        <br/>
+                        <input type="text" className="object" name="object"></input>
+                      </form>
+                    </div>
                     <div className="display-6">
                       <div><a className="fa fa-lg fa-github icons" id="icon-1" target="_blank" rel="noreferrer" href="https://www.github.com/petrucciii/">Github</a></div>
                       <div><a className="fa fa-lg fa-instagram icons" id="icon-2" target="_blank" rel="noreferrer" href="https://www.instagram.com/rickypetrucci.js/">Intagram</a></div>
