75f9dacf82805993268436b1662bd7647034481a
petrucciii <riccardo.petrucci99@gmail.com>
2021-04-08 22:08:53 +0200

    form-complete

--- app/src/components/contact.js
+++ app/src/components/contact.js
@@ -13,24 +13,66 @@ class Contact extends Component{
                     <img src={contact} alt="CONTATTI" style={{width: "100px", height: "100px"}}/>
                     <h4 className="card_title" align="center">CONTATTI</h4>
                     <div className="form">
-                      <form className="form-contact" action="">
+                      <form className="form-contact" >
                         <label className="text">Nome</label>
                         <br/>
-                        <input type="text" className="name" name="name"></input>
+                        <input 
+                          type="text" 
+                          className="name" 
+                          name="name"
+                        />
                         <br/>
                         <label className="text">E-Mail</label>
                         <br/>
-                        <input type="e-mail" className="e-mail" name="e-mail"></input>
+                        <input
+                          type="e-mail" 
+                          className="e-mail" 
+                          name="e-mail"
+                        />
                         <br/>
                         <label className="text">Oggetto</label>
                         <br/>
-                        <input type="text" className="object" name="object"></input>
+                        <textarea
+                          type="text" 
+                          className="txtarea" 
+                          name="txtarea">
+                        </textarea>
+                        <br/>
+                        <input
+                          type="submit"
+                          className="send-button"
+                          value="Invia"
+                          name="submit"
+                        />
                       </form>
                     </div>
                     <div className="display-6">
-                      <div><a className="fa fa-lg fa-github icons" id="icon-1" target="_blank" rel="noreferrer" href="https://www.github.com/petrucciii/">Github</a></div>
-                      <div><a className="fa fa-lg fa-instagram icons" id="icon-2" target="_blank" rel="noreferrer" href="https://www.instagram.com/rickypetrucci.js/">Intagram</a></div>
-                      <div><a className="fa fa-lg fa icons" id="icon-3" target="_blank" rel="noreferrer" href="mailto:riccardo.petrucci99@gmail.com">E-Mail</a></div>
+                      <div>
+                        <a 
+                          className="fa fa-lg fa-github icons" 
+                          id="icon-1" target="_blank" 
+                          rel="noreferrer" 
+                          href="https://www.github.com/petrucciii/">
+                            Github
+                        </a>
+                      </div>
+                      <div>
+                        <a 
+                          className="fa fa-lg fa-instagram icons" 
+                          id="icon-2" target="_blank" 
+                          rel="noreferrer" 
+                          href="https://www.instagram.com/rickypetrucci.js/">
+                            Intagram
+                        </a>
+                      </div>
+                      <div>
+                        <a 
+                          className="fa fa-lg fa icons" 
+                          id="icon-3" target="_blank" 
+                          rel="noreferrer" 
+                          href="mailto:riccardo.petrucci99@gmail.com">E-Mail
+                        </a>
+                      </div>
                     </div>
                 </div>
             </div>
