135b4a8bd8c866114e57e35512a10eb6fa2e82c3
petrucciii <riccardo.petrucci99@gmail.com>
2021-04-11 14:40:37 +0200

    responive

--- app/src/components/info.js
+++ app/src/components/info.js
@@ -25,7 +25,7 @@ class Info extends Component{
                 CHI SONO
               </h4>
               <div className="display-6">
-                Sono un programmatore web, ho 6 mesi di esperienza sono un autodidatta e programmo con React.js, Bootstrap, HTML e CSS per il Front-End e Node.js, PHP e Django per il Back-End.
+                Sono un programmatore web, programmo con React.js, Bootstrap, HTML e CSS per il Front-End e Node.js, PHP e Django per il Back-End.
                 Faccio principalmente siti vetrina.
               </div>
             </div>
