2c3b5ad174e4f5708e9e8d63e255ace33b19701e
petrucciii <riccardo.petrucci99@gmail.com>
2021-05-02 16:06:51 +0200

    router

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,28 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import './App.css';
 import {Helmet} from "react-helmet";
-//import Error404 from './pages/Error';
+import Error404 from './pages/Error';
 import HomeIt from './pages/HomeIt';
-//import HomeEn from './pages/HomeEn';
+import HomeEn from './pages/HomeEn';
 
 class App extends Component{
   render(){
     return (
-      <div>
-        <Helmet>
-          <title>Riccardo Petrucci</title>
-        </Helmet>
-        <HomeIt/>
-        {/*<Error404/>
-        <HomeEn/>*/}
-      </div>
+      <BrowserRouter>
+        <div>
+          <Helmet>
+            <title>Riccardo Petrucci</title>
+          </Helmet>
+          <Switch>
+            <Route exact path="/" component={HomeIt}/>
+            <Route exact path="/it" component={HomeIt}/>
+            <Route exact path="/en" component={HomeEn}/>
+            <Route exact path="/us" component={HomeEn}/>
+            <Route path="*" component={Error404}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
