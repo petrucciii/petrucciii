0f57907a1604bccdf296ebddf636dc7edba95bf2
petrucciii <riccardo.petrucci99@gmail.com>
2021-08-03 14:12:26 +0200

    restore

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,8 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import './App.css';
 import {Helmet} from "react-helmet";
-import Error404 from './pages/Error';
-import HomeIt from './pages/HomeIt';
-import HomeEn from './pages/HomeEn';
 
-class App extends Component{
+export default class App extends Component{
   render(){
     return (
       <BrowserRouter>
@@ -15,16 +11,10 @@ class App extends Component{
             <title>Riccardo Petrucci</title>
           </Helmet>
           <Switch>
-            <Route exact path="/" component={HomeIt}/>
-            <Route exact path="/it" component={HomeIt}/>
-            <Route exact path="/en" component={HomeEn}/>
-            <Route exact path="/us" component={HomeEn}/>
-            <Route path="*" component={Error404}/>
+            
           </Switch>
         </div>
       </BrowserRouter>
     );
   }
 }
-
-export default App;
